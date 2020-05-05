42713c253539ea512f631efc582e16fd058ff918
Brijesh Kumar <kumarbrijesh9021@gmail.com>
2020-05-06 02:23:01 +0530

    change alignment of segments

--- src/common/SevenSegDisplay.js
+++ src/common/SevenSegDisplay.js
@@ -6,45 +6,8 @@ const SevenSegDisplay = ({ segments = [] }) => {
   const on = "red";
 
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="150"
-      height="150"
-      viewBox="0 0 310.001 541.48"
-    >
+    <svg width="150" height="150" viewBox="0 0 310.001 541.48">
       <g id="Group_1" transform="translate(-15.758 -17.26)">
-        <path
-          id="G"
-          d="M278.759,287.927l-36,36h-144l-36-36,36-36h144Z"
-          fill={segments[6] ? on : off}
-          stroke={segments[6] ? on : off}
-          strokeMiterlimit="10"
-          strokeWidth="4"
-        />
-        <path
-          id="D"
-          d="M278.759,520.74l-36,36h-144l-36-36,36-36h144Z"
-          fill={segments[3] ? on : off}
-          stroke={segments[3] ? on : off}
-          strokeMiterlimit="10"
-          strokeWidth="4"
-        />
-        <path
-          id="C"
-          d="M287.759,295.928l36,36v144l-36,36-36-36v-144Z"
-          fill={segments[2] ? on : off}
-          stroke={segments[2] ? on : off}
-          strokeMiterlimit="10"
-          strokeWidth="4"
-        />
-        <path
-          id="E"
-          d="M53.758,295.928l36,36v144l-36,36-36-36v-144Z"
-          fill={segments[4] ? on : off}
-          stroke={segments[4] ? on : off}
-          strokeMiterlimit="10"
-          strokeWidth="4"
-        />
         <path
           id="A"
           d="M278.759,55.26l-36,36h-144l-36-36,36-36h144Z"
@@ -62,10 +25,42 @@ const SevenSegDisplay = ({ segments = [] }) => {
           strokeWidth="4"
         />
         <path
+          id="C"
+          d="M287.759,295.928l36,36v144l-36,36-36-36v-144Z"
+          fill={segments[2] ? on : off}
+          stroke={segments[2] ? on : off}
+          strokeMiterlimit="10"
+          strokeWidth="4"
+        />
+        <path
+          id="D"
+          d="M278.759,520.74l-36,36h-144l-36-36,36-36h144Z"
+          fill={segments[3] ? on : off}
+          stroke={segments[3] ? on : off}
+          strokeMiterlimit="10"
+          strokeWidth="4"
+        />
+        <path
+          id="E"
+          d="M53.758,295.928l36,36v144l-36,36-36-36v-144Z"
+          fill={segments[4] ? on : off}
+          stroke={segments[4] ? on : off}
+          strokeMiterlimit="10"
+          strokeWidth="4"
+        />
+        <path
           id="F"
           d="M53.758,64.427l36,36v144l-36,36-36-36v-144Z"
           fill={segments[5] ? on : off}
           stroke={segments[5] ? on : off}
+          strokeMiterlimit="10"
+          strokeWidth="4"
+        />
+        <path
+          id="G"
+          d="M278.759,287.927l-36,36h-144l-36-36,36-36h144Z"
+          fill={segments[6] ? on : off}
+          stroke={segments[6] ? on : off}
           strokeMiterlimit="10"
           strokeWidth="4"
         />
